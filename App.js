8eeaabba2eb72b76912bdfe7aee315c40ba2bc7b
ahmed7761 <76873374+ahmed7761@users.noreply.github.com>
2021-09-29 19:20:12 +0600

    useDimention hook

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Platform, StatusBar, TouchableOpacity, Alert, TouchableWithoutFeedback } from 'react-native';
+import { useDimensions, useDeviceOrientation } from "@react-native-community/hooks";
 
 export default function App() {
+    console.log(useDimensions())
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text numberOfLines={1}>Sharif Ipsum is dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.d</Text>
+      <TouchableOpacity onPress={() => Alert.alert('my title', 'message',[{text: 'yes', onPress: () => console.log('yes press')}, {text: 'no'}])}>
+        <Image source={{ height:300, width:200, uri: "https://picsum.photos/200" }} />
+      </TouchableOpacity>
     </View>
   );
 }
@@ -17,5 +21,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
